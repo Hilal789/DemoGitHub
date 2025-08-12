482677cc8c5732e68f608b2c844feebdc6ad3114
Hilal789 <smhilal789@gmail.com>
2025-08-12 14:53:02 +0530

    TL commit

---
+++ GitHubDemo-TL/src/main/java/com/nit/GitHubDemoTlApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubDemoTlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubDemoTlApplication.class, args);
+	}
+
+}
